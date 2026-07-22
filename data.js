362e0a59006436ebb946fa7b7270d26ad207b568
Rajesh rane <87178118+rane33@users.noreply.github.com>
2026-07-22 05:51:16 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,32 @@
+const birthdayData = {
 
+    name: "Your Friend",
+
+    heading: "🎉 Happy Birthday 🎉",
+
+    message: `
+Wishing you endless happiness,
+good health,
+success,
+love,
+and lots of beautiful memories.
+
+May every dream come true.
+
+Have an amazing birthday!
+
+❤️❤️❤️
+`,
+
+    photos: [
+        "images/photo1.jpg",
+        "images/photo2.jpg",
+        "images/photo3.jpg",
+        "images/photo4.jpg",
+        "images/photo5.jpg",
+        "images/photo6.jpg"
+    ],
+
+    music: "music/birthday.mp3"
+
+};
